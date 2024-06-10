8a827094fccbb7b7aae935b890a87ba5e4dd9579
Christine Tzani <95777233+christinetzani@users.noreply.github.com>
2024-06-10 12:58:50 +0000

    'Apply code formatting'

--- src/RandomPokemon.js
+++ src/RandomPokemon.js
@@ -1,12 +1,49 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./RandomPokemon.css";
 
 export default function RandomPokemon() {
-  const pokeNum = Math.floor(Math.random() * 151) + 1;
-  const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeNum}.png`;
+  const [pokemon, setPokemon] = useState(null);
+
+  const fetchPokemon = async () => {
+    const pokeNum = Math.floor(Math.random() * 151) + 1;
+    try {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${pokeNum}`,
+      );
+      setPokemon(response.data);
+    } catch (error) {
+      console.error("Error fetching Pokémon data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  if (!pokemon) {
+    return <div>Loading...</div>;
+  }
+
+  const { name, sprites } = pokemon;
+  const imageUrl = sprites.other["official-artwork"].front_default;
+
   return (
     <div className="RandomPokemon">
-      <h1>Pokemon #{pokeNum}</h1>
-      <img src={url} alt="" />
+      <div className="PokemonCard">
+        <h1>
+          Pokémon #{pokemon.id}: {name.charAt(0).toUpperCase() + name.slice(1)}
+        </h1>
+        <img src={imageUrl} alt={name} />
+        <div className="PokemonDetails">
+          <p>Height: {pokemon.height}</p>
+          <p>Weight: {pokemon.weight}</p>
+          <p>Base experience: {pokemon.base_experience}</p>
+        </div>
+      </div>
+      <button className="NextButton" onClick={fetchPokemon}>
+        Next Pokémon
+      </button>
     </div>
   );
 }
